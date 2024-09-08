5565e0756fc91a841ab630bcae2db36906edb37a
fernandomqs <fernandopozomqs@gmail.com>
2024-09-08 15:18:55 -0300

    adição Entity para banco

--- mercadolivro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ mercadolivro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -22,13 +22,13 @@ class CustomerService  {
             1
         }else{
             customers.last().id!!.toInt() + 1
-        }.toString()
+        }
 
         customer.id = id
         customers.add(customer)
     }
 
-    fun getByID(id: String): CustomerModel {
+    fun getByID(id: Int): CustomerModel {
         return customers.filter { it.id == id }.first()
     }
 
@@ -39,7 +39,7 @@ class CustomerService  {
         }
     }
 
-    fun delete(id: String) {
+    fun delete(id: Int) {
         customers.removeIf{it.id == id}
     }
 }

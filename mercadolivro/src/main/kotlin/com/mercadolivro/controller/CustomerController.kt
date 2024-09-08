1108608fdf3392855e8141f1ec93715e4724cc62
fernandomqs <fernandopozomqs@gmail.com>
2024-09-08 12:25:46 -0300

    admelhorias do service

--- mercadolivro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercadolivro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -4,6 +4,7 @@ import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.service.CustomerService
+import extension.toCustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -20,7 +21,7 @@ class CustomerController(
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest){
-       customerService.create(customer)
+       customerService.create(customer.toCustomerModel())
     }
 
     @GetMapping("/{id}")
@@ -31,7 +32,7 @@ class CustomerController(
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun update(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
-        customerService.update(id, customer)
+        customerService.update(customer.toCustomerModel(id))
     }
 
     @DeleteMapping("/{id}")

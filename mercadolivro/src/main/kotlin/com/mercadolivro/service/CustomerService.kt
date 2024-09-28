25542d63140b8fe338553f3ded559d3e3fdcb0ac
fernandomqs <fernandopozomqs@gmail.com>
2024-09-28 14:55:39 -0300

    adicionando spring security

--- mercadolivro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ mercadolivro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -6,13 +6,15 @@ import com.mercadolivro.enums.Errors
 import com.mercadolivro.enums.Profile
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.repository.CustomerRepository
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class CustomerService(
     private val customerRepository: CustomerRepository,
-    val bookService: BookService
-)  {
+    private val bookService: BookService,
+    private val bCrypt: BCryptPasswordEncoder
+) {
 
 
     fun getAll(name: String?): List<CustomerModel>{
@@ -24,7 +26,8 @@ class CustomerService(
 
     fun create(customer: CustomerModel){
         val customerCopy = customer.copy(
-            roles = setOf(Profile.CUSTOMER)
+            roles = setOf(Profile.CUSTOMER),
+            password = bCrypt.encode(customer.password)
         )
        customerRepository.save(customerCopy)
     }
